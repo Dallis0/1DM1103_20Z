5c33f1b91f866c28080e7b5d7c7dc2c6134d9ef1
Dallis0 <marcin.pietrzak0157@gmail.com>
2020-10-22 16:42:24 +0200

    Zad1

--- sem3/zad1.c
+++ sem3/zad1.c
@@ -4,7 +4,8 @@
 int main(int argc, char* argv[]){
    
    float a = atof(argv[1]);
-   printf("Wartość %0.0f do kwadratu to %0.0f\n", a, a*a);
+   printf("Wartość %0.0f do kwadratu to %0.0f.\n", a, a*a);
+
 }
 
 
