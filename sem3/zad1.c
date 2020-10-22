d5d16622640454f6020432d9d66d92794e29b1aa
Dallis0 <marcin.pietrzak0157@gmail.com>
2020-10-22 16:37:32 +0200

    Zad1

---
+++ sem3/zad1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+   
+   float a = atof(argv[1]);
+   printf("Wartość %0.0f do kwadratu to %0.0f\n", a, a*a);
+}
+
+
+
+
